9df655e4808557813ff5d43d0f209a7944626887
Mohsin Latif <mohsinlatifvirk@gmail.com>
2018-08-26 20:23:41 +0500

    pwa settings

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, "..", "public");
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+const PORT = process.env.PORT || 3000;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log('Server is up!');
+app.listen(PORT, () => {
+  console.log("Server is up!", `runing on port:${PORT}`);
 });
